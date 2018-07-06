339f4660ff9b483c7ce9511be64502beec16e756
bambangramadhan <bambangramadhan96@gmail.com>
2018-07-06 17:25:08 +0700

    stringManipulation done

---
+++ c5/string.js
@@ -0,0 +1,12 @@
+function stringManipulation(word){
+  if (word[0] == 'a' || word[0] == 'i' || word[0] == 'u' || word[0] == 'e' ||
+  word[0] == 'o'){
+    return word;
+  } else {
+    return `${word.substring(1, word.length)}${word[0]}nyo`;
+  }
+
+}
+
+console.log(stringManipulation('ayam'));
+console.log(stringManipulation('bebek'));
