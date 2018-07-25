4d7acf22c35477a1be141e5bdc1b8abf9c905b19
bambangramadhan <bambangramadhan96@gmail.com>
2018-07-25 09:52:00 +0700

    todo done

---
+++ c13/todo.js
@@ -0,0 +1,12 @@
+console.log('>>> JS TODO <<<');
+console.log('$ node todo.js <command>');
+console.log('$ node todo.js list');
+console.log('$ node todo.js task <task_id>');
+console.log('$ node todo.js add <task_content>');
+console.log('$ node todo.js delete <task_id>');
+console.log('$ node todo.js complete <task_id>');
+console.log('$ node todo.js uncomplete <task_id>');
+console.log('$ node todo.js list:outstanding asc|desc');
+console.log('$ node todo.js list:completed asc|desc');
+console.log('$ node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>');
+console.log('$ node todo.js filter <tag_name>');
